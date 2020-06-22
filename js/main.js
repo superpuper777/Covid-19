376fe2923d387ab2adbbc00d22378471f043014d
superpuper777 <superfevrik@gmail.com>
2020-06-23 00:09:06 +0300

    Add styles for ipad's screen

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ let circle = document.createElement("div");
 circle.style.cssText = `width: 6px; height: 6px; margin: 0 auto ; background-color: #FA5652; border-radius: 100%;`;
 
 headerLinks.forEach(link => link.addEventListener("click", function () {
-  console.log("click");
   link.classList.add("highlighter");
   setTimeout(() => link.classList.remove("highlighter"), 2000);
   link.after(circle);
